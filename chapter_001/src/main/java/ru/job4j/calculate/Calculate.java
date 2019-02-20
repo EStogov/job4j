efb9d046c8ea2edc734a696fe12007952f76cf88
Egor Stogov <egstegst@yandex.ru>
2019-02-20 14:37:30 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate.
+* @author Egor Stogov (egst@rambler.ru)
+* @version $Id$
+* @since 0.1
+*/
+
 public class Calculate {
+	
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	
 	public static void main (String[] args){
 		System.out.println("Hello world!");
