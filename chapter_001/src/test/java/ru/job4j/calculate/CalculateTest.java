051f266fdb259736481bbc5f07c230ddc9ba6ed1
Egor Stogov <egstegst@yandex.ru>
2019-02-21 18:17:52 +0300

    2.5. Инспектор кода IDEA. [#69757]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,25 +5,25 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Egor Stogov (egst@rambler.ru)
-* @version $Id$
-* @since 0.1
-*/
-	public class CalculateTest {
-	
+ * Test.
+ *
+ * @author Egor Stogov (egst@rambler.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+
 	/**
-	* Test echo.
-	*/
-	
+	 * Test echo.
+	 */
+
 	@Test
 	public void whenTakeNameThenThreeEchoPlusName() {
 		String input = "Egor Stogov";
-		String expect = "Echo, echo, echo : Egor Stogov"; 
+		String expect = "Echo, echo, echo : Egor Stogov";
 		Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
 	}
- 
+
 }
