a8bcfa8dad799fc63317e50b7837cb21c071a9f4
Egor Stogov <mailto:egstegst@yandex.ru>
2019-02-22 13:53:42 +0300

    3.2. Конвертер валюты. [#41583]

---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Egor Stogov (egst@rambler.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+
+public class ConverterTest {
+
+    /**
+     * Test convert to dollar.
+     */
+
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test convert to euro.
+     */
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+}
