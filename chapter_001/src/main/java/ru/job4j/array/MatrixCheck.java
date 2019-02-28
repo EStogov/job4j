cd0ddab09ae306e5f50c0da574f98c83d53e7d4f
Egor Stogov <egstegst@yandex.ru>
2019-02-28 14:19:03 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям. [#53859]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * @author Egor Stogov (egst@rambler.ru)
+ * @version &Id&
+ * @since 0.1
+ */
+
+public class MatrixCheck {
+
+    /**
+     * Метод проверки матрицы на монотонность по диагонали.
+     * @param data - матрица
+     * @return результат
+     */
+
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i <= data.length - 1; i++) {
+            if ((data[i][i] != data[0][0]) || (data[i][data.length - 1 - i] != data[0][data.length - 1])) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
