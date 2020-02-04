af2e90bb07912faece5236c9311741a90beeb5e4
Egor Stogov <egstegst@yandex.ru>
2020-02-04 13:08:37 +0300

    Исправление внезапных ошибок с кофемашиной.

--- chapter_002/src/main/java/ru/job4j/coffee/Coffee.java
+++ chapter_002/src/main/java/ru/job4j/coffee/Coffee.java
@@ -9,26 +9,31 @@ import java.util.Arrays;
  */
 
 public class Coffee {
-	
-	private final int[] coins = new int[] {10, 5, 2, 1};
-	
-	/**
+
+    private final int[] coins = new int[]{10, 5, 2, 1};
+
+    /**
      * Метод, реализующий выдачу сдачи.
+     *
      * @param value купюра
      * @param price цена стаканчика кофе
      * @return результат
      */
-	
-	public int[] changes(int value, int price) {
-		int change = value - price;
-		int[] result = new int[1];
-		for (int i = 0; i <= coins.length - 1; i++) {
-			for (int j = 0; j <= change / this.coins[i] - 1; j++) {
-				result = Arrays.copyOf(result, result.length + 1);
-				result[j] = coins[i];
-			}
-			change = change % this.coins[i];
-		}
-		return result;
-	}
+
+    public int[] changes(int value, int price) {
+        int change = value - price;
+        int count = 0;
+        int[] result = new int[1000];
+        for (int coin : this.coins) {
+            for (int i = 0; i <= change / coin - 1; i++) {
+                result[count] = coin;
+                count++;
+            }
+            change = change % coin;
+        }
+        if (count == 0) {
+            count = 1;
+        }
+        return Arrays.copyOf(result, count);
+    }
 }
